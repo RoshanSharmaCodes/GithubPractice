08a098bd0f4e4a08ce4265f3f8843aeca9078ac5
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2022-10-16 16:46:00 +0530

    DEVLOPEMNT CHANGE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Git Hub Practice -- Final Edit in Development
+          Git Hub Practice -- Final Edit in MAINNNNNN
         </p>
         <a
           className="App-link"
